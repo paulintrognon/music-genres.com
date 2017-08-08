ac99ee99edb553e236583b32f7b48c706ab6a958
Paulin Trognon <paulin.trognon@gmail.com>
2017-08-08 12:29:27 +0200

    fix security issue

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import './footer.css';
 
 export default function () {
   return (
     <footer className="main-footer">
-      <a href="https://github.com/paulintrognon/" target="_blank">
+      <a href="https://github.com/paulintrognon/" target="_blank" rel="noopener noreferrer">
         Paulin Trognon
       </a>
       &nbsp;-&nbsp;
-      <a href="https://github.com/paulintrognon/music-genres.com" target="_blank">
+      <a href="https://github.com/paulintrognon/music-genres.com" target="_blank" rel="noopener noreferrer">
         <i className="fa fa-github" aria-hidden="true"></i>
         &nbsp;Code source
       </a>
