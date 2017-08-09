cb3c7e71f87d092ce657b2f9bbf9aaf1930e472d
Paulin Trognon <paulin.trognon@gmail.com>
2017-08-09 14:09:17 +0200

    add uniq constraint on genre name

--- api/models/MusicGenre.js
+++ api/models/MusicGenre.js
@@ -4,9 +4,19 @@ const sequelize = require('../db.js').sequelize;
 const MusicGenre = sequelize.define('music_genre', {
   name: {
     type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: true,
+    len: [2, 100],
   },
+}, {
+  indexes: [
+    {
+      unique: true,
+      fields: ['name'],
+    },
+  ],
 });
 
-MusicGenre.sync();
+MusicGenre.sync({ alter: true });
 
 module.exports = MusicGenre;
