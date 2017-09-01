a29da15343d42c713492db89283c3443af72fbb7
Paulin Trognon <paulin.trognon@gmail.com>
2017-09-01 13:38:42 +0200

    add youtube service detection

---
+++ api/services/musicPlayer.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = createService();
+
+function createService() {
+  const service = {};
+  const detections = [
+    {
+      detect: /youtube\.com/,
+      extract: /\?v=(.{11})/,
+      service: 'youtube',
+    },
+    {
+      detect: /youtu\.be/,
+      extract: /youtu\.be\/(.{11})/,
+      service: 'youtube',
+    },
+  ];
+
+  service.parseTrackUrl = parseTrackUrl;
+
+  return service;
+
+  // ------------------------------------------------------
+
+  function parseTrackUrl(url) {
+    const detection = detections.find(detectionTested => {
+      return url.search(detectionTested.detect) !== -1;
+    });
+    if (detection) {
+      return extractYoutubeId(url, detection.extract);
+    }
+    return false;
+  }
+
+  function extractYoutubeId(url, regex) {
+    const matches = url.match(regex);
+    const id = matches[1];
+    return {
+      id,
+      service: 'youtube',
+    };
+  }
+}
---
+++ api/tests/services/musicPlayer.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const proxyquire = require('proxyquire').noCallThru();
+const should = require('should');
+
+const service = proxyquire('../../services/musicPlayer.js', {});
+
+describe('musicPlayer service', () => {
+  describe('.parseTrackUrl', parseTrackUrlSuite);
+});
+
+function parseTrackUrlSuite() {
+  describe('youtube', youtubeTests);
+
+  function youtubeTests() {
+    const expectedResult = {
+      service: 'youtube',
+      id: 'DwpedKWwS3w',
+    };
+    const urls = [
+      'https://www.youtube.com/watch?v=DwpedKWwS3w',
+      'http://www.youtube.com/watch?v=DwpedKWwS3w&t=50',
+      'https://youtu.be/DwpedKWwS3w',
+      'http://youtu.be/DwpedKWwS3w&t=50',
+      'youtube.com/watch?v=DwpedKWwS3w',
+      'youtu.be/DwpedKWwS3w&t=50',
+    ];
+
+    test(urls, expectedResult);
+  }
+
+  function test(urls, expectedResult) {
+    urls.forEach(url => {
+      it(`should detect ${JSON.stringify(expectedResult)} from ${url}`, () => {
+        const result = service.parseTrackUrl(url);
+        should(result).eql(expectedResult);
+      });
+    });
+  }
+}
