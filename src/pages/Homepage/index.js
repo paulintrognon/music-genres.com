e243702333c313ec8d222e3480cf205ba864114f
Paulin Trognon <paulin.trognon@gmail.com>
2017-10-08 18:25:47 +0200

    add magnifying glass to search intpu

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import './homepage.css';
 import or from './or.png';
+import magnifyingGlass from './magnifying-glass.png';
 
 export default class Homepage extends React.Component {
   render() {
     return (
       <div className="homepage-container">
         <p className="search-container">
-          <input type="text" className="search-input" placeholder="Look for a genre" />
+          <span className="search-input-container">
+            <input type="text" className="search-input" placeholder="Look for a genre" />
+            <img src={magnifyingGlass} className="search-icon" />
+          </span>
         </p>
         <p className="or-container">
           <img src={or} alt="or" />
