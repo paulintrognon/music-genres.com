0c791208c719c29d72398b64990647b123d1ddf0
Paulin Trognon <paulin.trognon@gmail.com>
2017-10-08 17:42:49 +0200

    integration of homepage

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import './homepage.css';
+import or from './or.png';
+
 export default class Homepage extends React.Component {
   render() {
     return (
-      <p>super homepage</p>
+      <div className="homepage-container">
+        <p className="search-container">
+          <input type="text" className="search-input" placeholder="Look for a genre" />
+        </p>
+        <p className="or-container">
+          <img src={or} alt="or" />
+        </p>
+        <p className="random-container">
+          <button className="random-button">
+            Get surprised
+          </button>
+        </p>
+      </div>
     );
   }
 }
