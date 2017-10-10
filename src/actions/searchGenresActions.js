c6addde07610796407c61a4a5847949e29c88af6
Paulin Trognon <paulin.trognon@gmail.com>
2017-10-10 22:17:41 +0200

    call search api in suggestGenreAction

--- src/actions/searchGenresActions.js
+++ src/actions/searchGenresActions.js
@@ -1,3 +1,5 @@
+import api from '../services/api';
+
 export function changeFocusAction(focus) {
   if (focus) {
     return { type: 'SEARCH_GENRE_FOCUS' };
@@ -8,5 +10,7 @@ export function changeFocusAction(focus) {
 export function suggestGenresAction(text) {
   return dispatch => {
     console.log('suggest', text);
+    api.get(`/music-genres/search?query=${text}`)
+      .then(res => console.log(res));
   };
 }
