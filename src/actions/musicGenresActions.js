da84407e37046af7fd11898db75d2d0532de500e
Paulin Trognon <paulin.trognon@gmail.com>
2017-10-17 20:59:11 +0200

    reset suggestions on goToGenre

--- src/actions/musicGenresActions.js
+++ src/actions/musicGenresActions.js
@@ -3,5 +3,7 @@ import { push } from 'react-router-redux';
 export function goToGenre(slug) {
   return dispatch => {
     dispatch(push(`/${slug}`));
+    dispatch({ type: 'SEARCH_GENRE_UNFOCUS' });
+    dispatch({ type: 'SEARCH_GENRE_SUGGESTIONS_RESET' });
   };
 }
