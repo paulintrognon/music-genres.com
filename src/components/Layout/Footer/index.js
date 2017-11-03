be90296a79b9e8a93ff1d06b7b4da583c0cabc19
Paulin Trognon <paulin.trognon@gmail.com>
2017-11-03 19:24:43 +0100

    fix style & better footer

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 
 import './footer.css';
 
+import plusImage from '../../../images/plus.png';
+
 export default class Footer extends React.Component {
   render() {
     return (
       <footer className="main-footer">
-        Design by&nbsp;
-        <a href="https://fr.linkedin.com/in/margotbrun" target="_blank" rel="noopener noreferrer">
-          Margot Brun
-        </a>
-        &nbsp;-&nbsp;
-        <a href="https://github.com/paulintrognon/music-genres.com" target="_blank" rel="noopener noreferrer">
-          Code by&nbsp;
-        </a>
-        <a href="https://paulintrognon.fr/" target="_blank" rel="noopener noreferrer">
-          Paulin Trognon
-        </a>
+        <div className="main-footer-content">
+          Design by&nbsp;
+          <a href="https://fr.linkedin.com/in/margotbrun" target="_blank" rel="noopener noreferrer">
+            Margot Brun
+          </a>
+          &nbsp;<img src={plusImage} className="plus-sign" />&nbsp;
+          <a href="https://github.com/paulintrognon/music-genres.com" target="_blank" rel="noopener noreferrer">
+            Code by&nbsp;
+          </a>
+          <a href="https://paulintrognon.fr/" target="_blank" rel="noopener noreferrer">
+            Paulin Trognon
+          </a>
+        </div>
       </footer>
     );
   }
