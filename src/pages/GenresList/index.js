7104fb5e54c052791214a3a18991223d0728264f
Paulin Trognon <paulin.trognon@gmail.com>
2018-02-13 20:14:24 +0100

    remove useless var

--- src/pages/GenresList/index.js
+++ src/pages/GenresList/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './styles.css';
 
 import { getAllGenres } from '../../services/api';
-import { goToMusicGenre } from '../../actions/navigationActions';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import HashtagTitle from '../../components/HashtagTitle';
