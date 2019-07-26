6725f2ab4c0a81fcc67dfceaed87f84b97582866
Paulin Trognon <paulin.trognon@gmail.com>
2019-07-26 17:21:25 +1200

    remove useless migration lines

--- server/db/migrations/2.sql
+++ server/db/migrations/2.sql
@@ -1,8 +1,2 @@
 ALTER TABLE `votes` DROP `deletedAt`;
 ALTER TABLE `musicGenreTracks` DROP `deletedAt`;
-
-ALTER TABLE `MusicGenreParents` ROW_FORMAT = COMPACT;
-ALTER TABLE `MusicGenres` ROW_FORMAT = COMPACT;
-ALTER TABLE `MusicGenreTracks` ROW_FORMAT = COMPACT;
-ALTER TABLE `Tracks` ROW_FORMAT = COMPACT;
-ALTER TABLE `Votes` ROW_FORMAT = COMPACT;
