7e83d1ead7894be4834c0197b8548e9f016743ae
Paulin Trognon <contact@paulintrognon.fr>
2020-12-04 10:08:49 +0100

    Pwa: Add Api Test suite

--- pwa/src/services/api/parseYoutubeUrl.ts
+++ pwa/src/services/api/parseYoutubeUrl.ts
@@ -1,6 +1,6 @@
 import { YoutubeParsedTrack } from '../../types/Track/YoutubeParsedTrack'
 import { HttpResponse, post } from './api'
 
-export async function parseUrl(url: string): Promise<HttpResponse<YoutubeParsedTrack>> {
-  return post<YoutubeParsedTrack>('/api/tracks/parseUrl', { trackUrl: url })
+export async function parseUrl(trackUrl: string): Promise<HttpResponse<YoutubeParsedTrack>> {
+  return post<YoutubeParsedTrack>('/api/tracks/parseUrl', { trackUrl })
 }
